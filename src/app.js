cb11721c2a0b497144f0e7f639a02af26fb1b1e1
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-03-15 08:44:10 -0600

    Agregar soporte para datos del request en JSON.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const routes = require('./routes/routes');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(routes);
 
 module.exports = app;
