12116981df0d6feb20d1376e4b1163c889a1f3ea
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-04-11 09:35:37 -0600

    Agregar configuración de TailwindCSS.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,11 +3,16 @@ import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/new' element={<PostForm/>} />
-      <Route path='*' element={<NotFound/>} />
-    </Routes>
+    <div className='bg-neutral-900 min-h-screen flex items-center'>
+      <div className='container p-10 bg-neutral-500 m-auto'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/new' element={<PostForm/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+
+      </div>
+    </div>
   );
 }
 
