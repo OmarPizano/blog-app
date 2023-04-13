0cec8e0cbb95922a432776ef4fd519dd68bd393c
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-04-13 16:26:23 -0600

    Agregar CORS para solucionar error con AXIOS al hacer el request entre contenedores Docker.

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const uploader = require('express-fileupload')
 const app = express();
 const routes = require('./routes/routes');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(uploader({
