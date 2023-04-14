0f4a538f9a7349058b67bbd47d8076a2d523bb92
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-04-14 08:16:35 -0600

    Agregar el listado de publicaciopnes.

--- src/frontend/src/context/PostContext.js
+++ src/frontend/src/context/PostContext.js
@@ -1,16 +1,27 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useEffect } from 'react';
+import { apiGetPosts } from "../api/posts";
 
 const post_context = createContext();
 
-export const getContext = () => {
+export const GetContext = () => {
     const context = useContext(post_context);
     return context;
 }
 
 export const PostProvider = ({children}) => {
     const [posts, setPosts] = useState([]);
+
+    const getPosts = async () => {
+        const res = await apiGetPosts();
+        setPosts(res.data);
+    }
+
+    useEffect(() => {
+      getPosts(); 
+    }, [])
+    
     return (
-        <post_context.Provider value={ {posts, setPosts} }>
+        <post_context.Provider value={ {posts, setPosts, getPosts} }>
             {children}
         </post_context.Provider>
     );
--- src/frontend/src/pages/Home.js
+++ src/frontend/src/pages/Home.js
@@ -1,5 +1,27 @@
+import { GetContext } from "../context/PostContext";
+import { AiFillFileExclamation } from "react-icons/ai";
+
 export function Home() {
+    const {posts} = GetContext();
+
+    if (posts.length === 0) return (
+        <div className="flex flex-col justify-center items-center">
+            <AiFillFileExclamation className="w-24 h-24 text-white" />
+            <h1 className="text-white">There is nothing here yet!</h1>
+        </div>
+    )
+
     return (
-        <div>Home Page</div>
+        <div className="text-white">
+            <h1>Posts</h1>
+            <hr />
+            {posts.map(post => (
+                <div key={post._id}>
+                    <h1>{post.title}</h1>
+                    <p>{post.date}</p>
+                    <p>{post.content}</p>
+                </div>
+            ))}
+        </div>
     );
 };
