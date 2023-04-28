1708a9db9ebf0018a4c0edf32eed6430e5f71a54
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-04-27 18:48:06 -0600

    Establecer valor por defecto para el puerto (nube).

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv');
 
 exports.MONGO_URI = process.env.MONGO_URI;
-exports.BACKEND_PORT = process.env.BACKEND_PORT;
+exports.BACKEND_PORT = process.env.BACKEND_PORT || 80;
 exports.CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
 exports.CLOUDINARY_KEY = process.env.CLOUDINARY_KEY;
 exports.CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET;
